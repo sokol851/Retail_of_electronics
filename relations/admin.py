b2922274c5ad46961394d7ea190547d797b02826
Ilia Akatov <ilia-akatoff@ya.ru>
2024-12-17 00:33:51 +0300

    Create of models

---
+++ relations/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from relations.models import Partner, Contact, Product
+
+
+@admin.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "contact",
+        "supplier",
+        "debt",
+    )
+    list_filter = ('contact__city',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "country",
+        "city",
+        "street",
+        "house_number",
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "model",
+        "release_date",
+        "partner",
+    )
---
+++ relations/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Contact(models.Model):
+    email = models.EmailField(verbose_name='Почта')
+    country = models.CharField(max_length=75, verbose_name='Страна')
+    city = models.CharField(max_length=75, verbose_name='Город')
+    street = models.CharField(max_length=75, verbose_name='Улица')
+    house_number = models.CharField(max_length=10, verbose_name='Номер дома')
+
+    def __str__(self):
+        return f'{self.email} ({self.country})'
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+
+class Partner(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE,
+                                null=True, blank=True,
+                                verbose_name='Контакты')
+    supplier = models.ForeignKey("self", on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 verbose_name='Предыдущий поставщик')
+    debt = models.DecimalField(max_digits=20, decimal_places=2, default=0,
+                               verbose_name='Задолженность')
+    create_at = models.DateTimeField(auto_now_add=True,
+                                     verbose_name='Время создания')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Партнёр'
+        verbose_name_plural = 'Партнёры'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    model = models.CharField(max_length=75, verbose_name='Модель')
+    release_date = models.DateField(verbose_name='Дата выпуска')
+    partner = models.ForeignKey(Partner, on_delete=models.CASCADE,
+                                verbose_name='Партнёр')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
